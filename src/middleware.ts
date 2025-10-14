c90a3dc5cf910181d677ad98be4e5e8f5cd7e18e
Avenida Legal <avenidalegal.2024@gmail.com>
2025-10-14 11:02:41 -0600

    fix: Simplify middleware to use default next-auth/middleware behavior

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,4 @@
-import { withAuth } from "next-auth/middleware";
-
-export default withAuth(
-  function middleware(req) {
-    // This function is called for all matched routes
-    // The authentication check is handled by withAuth
-  },
-  {
-    callbacks: {
-      authorized: ({ token, req }) => {
-        // Allow access to /client/* routes only if user is authenticated
-        if (req.nextUrl.pathname.startsWith('/client')) {
-          return !!token;
-        }
-        // Allow all other routes
-        return true;
-      },
-    },
-  }
-);
+export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: [
